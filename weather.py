d459d1cd321c255d409922e9e25bef6d35aa256d
drod75 <dr507498@gmail.com>
2024-11-08 10:34:49 -0500

    testing

---
+++ weather.py
@@ -0,0 +1,13 @@
+from ratelimit import limits
+from datetime import datetime, timedelta
+from pymeteosource.api import Meteosource
+from pymeteosource.types import tiers, sections, langs, units
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path='.env')
+
+def get_weather():
+    meteosource = Meteosource(os.getenv('metoeosource'), tier=tiers.FREE)
+    forecast = meteosource.get_point_forecast(place_id=place, units=units.US)
+    return forecast
